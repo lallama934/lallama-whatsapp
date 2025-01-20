ad6d7feee039649ecaac2223113646d763e366d3
Jeison Llanos <lallamaexpendio3@gmail.com>
2025-01-20 06:31:07 -0500

    update token

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,7 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 app.use(express.static('tmp'))
-app.use(`/`,routes)
+app.use(`/get/`,routes)
 app.get('/', (req: any, res: { send: (arg0: string) => void }) => {
     res.send('Hola mi server en express');
 });
