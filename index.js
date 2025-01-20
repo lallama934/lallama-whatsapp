8b0621d97f6e40229fa2166fd0ecb6baaefee183
Jeison Llanos <lallamaexpendio3@gmail.com>
2025-01-20 06:18:20 -0500

    update vercel

--- index.js
+++
@@ -1,9 +0,0 @@
-const {createApp} = require('./src/app');
-
-(async () => {
-  const port = process.env.PORT || 3001;
-  const app = await createApp();
-  app.listen(port, () => {
-    console.log(`Mi port ${port}`);
-  });
-})();
--- src/app.ts
+++ src/app.ts
@@ -2,20 +2,11 @@ import "dotenv/config"
 import express from "express"
 import cors from "cors"
 import routes from "./infrastructure/router"
+const port = process.env.PORT || 3001
+const app = express()
+app.use(cors())
+app.use(express.json())
+app.use(express.static('tmp'))
+app.use(`/`,routes)
 
-
-const createApp = async () => {
-    const app = express()
-    app.get('/', (req: any, res: { send: (arg0: string) => void }) => {
-        res.send('Hola mi server en express');
-    });
-    app.use(cors())
-    app.use(express.json())
-    app.use(express.static('tmp'))
-    app.use(`/`, routes)
-
-    return app;
-}
-
-export {createApp};
-
+app.listen(port, () => console.log(`Ready...${port}`))
